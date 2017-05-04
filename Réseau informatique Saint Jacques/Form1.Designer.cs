325b73f7493192945c60061b57a98396d6a24288
User <User@EDGE-ADMIN>
2017-05-04 15:12:02 +0200

    15:11

--- Réseau informatique Saint Jacques/Form1.Designer.cs
+++ Réseau informatique Saint Jacques/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.Tables = new System.Windows.Forms.ComboBox();
+            this.Salles = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Videoprojecteurs = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // Tables
@@ -39,14 +42,42 @@
             this.Tables.Size = new System.Drawing.Size(121, 21);
             this.Tables.TabIndex = 0;
             // 
+            // Salles
+            // 
+            this.Salles.FormattingEnabled = true;
+            this.Salles.Location = new System.Drawing.Point(511, 123);
+            this.Salles.Name = "Salles";
+            this.Salles.Size = new System.Drawing.Size(121, 21);
+            this.Salles.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(512, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 238);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Videoprojecteurs
+            // 
+            this.Videoprojecteurs.FormattingEnabled = true;
+            this.Videoprojecteurs.Location = new System.Drawing.Point(730, 123);
+            this.Videoprojecteurs.Name = "Videoprojecteurs";
+            this.Videoprojecteurs.Size = new System.Drawing.Size(121, 21);
+            this.Videoprojecteurs.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1322, 648);
+            this.Controls.Add(this.Videoprojecteurs);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Salles);
             this.Controls.Add(this.Tables);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -54,6 +85,9 @@
         #endregion
 
         private System.Windows.Forms.ComboBox Tables;
+        private System.Windows.Forms.ComboBox Salles;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox Videoprojecteurs;
     }
 }
 
--- Réseau informatique Saint Jacques/Form1.cs
+++ Réseau informatique Saint Jacques/Form1.cs
@@ -11,25 +11,68 @@ namespace Réseau_informatique_Saint_Jacques
         public Form1()
         {
             InitializeComponent();
-            Combobox_tables();           
-        } 
-        
+           
+        }        
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+           
+            Combobox_tables();
+            Combobox_imprimantes();
+            Listbox_test();
+            Combobox_Videoprojecteurs();
+        }
+
+        private void Combobox_imprimantes()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Reseau St Jacques.accdb";
+            string requete = "SELECT DISTINCT Modèle FROM Imprimantes ORDER BY Modèle";
+            OleDbDataAdapter dAdapter = new OleDbDataAdapter(requete, connectionString);
+            DataTable source = new DataTable();
+            dAdapter.Fill(source);
+            Salles.DataSource = source;
+            Salles.DisplayMember = "Modèle";
+            Salles.ValueMember = "Modèle";
+        }
         private void Combobox_tables()
         {
-            string connetionString = null;
-            OleDbConnection database;
-            connetionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Reseau St Jacques.accdb";
-            database = new OleDbConnection(connetionString);
-            database.Open();
-            DataTable userTables = null;
-            userTables = database.GetSchema("Tables");
+                        
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Reseau St Jacques.accdb";
+            OleDbConnection database = new OleDbConnection(connectionString);
+            database.Open();            
+            DataTable userTables = database.GetSchema("Tables");
 
             List<string> tableNames = new List<string>();
             for (int i = 0; i < userTables.Rows.Count; i++)
                 if (!(userTables.Rows[i][2].ToString()).Contains("MS"))
                     Tables.Items.Add(userTables.Rows[i][2].ToString());
             database.Close();
-        }        
-       
+            Tables.SelectedIndex = 0;
+        }
+
+        private void Listbox_test()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Reseau St Jacques.accdb";
+            string requete = "SELECT Salle FROM Vidéoprojecteurs WHERE Modèle = 'Optoma EX610ST' ORDER BY Salle";
+            OleDbDataAdapter dAdapter = new OleDbDataAdapter(requete, connectionString);
+            DataTable source = new DataTable();
+            dAdapter.Fill(source);
+            listBox1.DataSource = source;
+            listBox1.DisplayMember = "Salle";
+            listBox1.ValueMember = "Salle";
+        }
+
+        private void Combobox_Videoprojecteurs()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= C:\\Reseau St Jacques.accdb";
+            string requete = "SELECT DISTINCT Modèle FROM Vidéoprojecteurs ORDER BY Modèle";
+            OleDbDataAdapter dAdapter = new OleDbDataAdapter(requete, connectionString);
+            DataTable source = new DataTable();
+            dAdapter.Fill(source);
+            Videoprojecteurs.DataSource = source;
+            Videoprojecteurs.DisplayMember = "Modèle";
+            Videoprojecteurs.ValueMember = "Modèle";
+        }
+
     }
 }
