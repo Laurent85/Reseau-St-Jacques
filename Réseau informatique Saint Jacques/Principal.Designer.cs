8addfc55d7e9b2839b7ffc5c68ee462b1d85b263
User <User@EDGE-ADMIN>
2017-05-11 16:50:03 +0200

    16:49

--- Réseau informatique Saint Jacques/Principal.Designer.cs
+++ Réseau informatique Saint Jacques/Principal.Designer.cs
@@ -32,18 +32,33 @@
             this.Salles = new System.Windows.Forms.ComboBox();
             this.Liste_départ = new System.Windows.Forms.ListBox();
             this.Videoprojecteurs = new System.Windows.Forms.ComboBox();
-            this.Videoprojecteur = new System.Windows.Forms.TextBox();
-            this.Date_releve = new System.Windows.Forms.TextBox();
-            this.Nombre_heures = new System.Windows.Forms.TextBox();
+            this.Date_relevé = new System.Windows.Forms.TextBox();
+            this.Heures_Lampe = new System.Windows.Forms.TextBox();
             this.Liste_périphériques = new System.Windows.Forms.ListBox();
             this.Ordinateurs = new System.Windows.Forms.ComboBox();
             this.Choix_colonne = new System.Windows.Forms.ComboBox();
+            this.Switch = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.Salle = new System.Windows.Forms.TextBox();
+            this.Bandeau = new System.Windows.Forms.TextBox();
+            this.VLAN = new System.Windows.Forms.TextBox();
+            this.Périphérique = new System.Windows.Forms.TextBox();
+            this.Modèle_périphérique = new System.Windows.Forms.TextBox();
+            this.Type = new System.Windows.Forms.TextBox();
+            this.Adresse_ip = new System.Windows.Forms.TextBox();
+            this.Imprimante = new System.Windows.Forms.TextBox();
+            this.Port_imprimante = new System.Windows.Forms.TextBox();
+            this.Type_imprimante = new System.Windows.Forms.TextBox();
+            this.Vidéoprojecteur = new System.Windows.Forms.TextBox();
+            this.Observations = new System.Windows.Forms.TextBox();
+            this.Modèle_Lampe = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // Tables
             // 
             this.Tables.FormattingEnabled = true;
-            this.Tables.Location = new System.Drawing.Point(479, 124);
+            this.Tables.Location = new System.Drawing.Point(559, 53);
             this.Tables.Name = "Tables";
             this.Tables.Size = new System.Drawing.Size(121, 21);
             this.Tables.TabIndex = 0;
@@ -51,7 +66,7 @@
             // Salles
             // 
             this.Salles.FormattingEnabled = true;
-            this.Salles.Location = new System.Drawing.Point(667, 124);
+            this.Salles.Location = new System.Drawing.Point(697, 53);
             this.Salles.Name = "Salles";
             this.Salles.Size = new System.Drawing.Size(121, 21);
             this.Salles.TabIndex = 1;
@@ -68,34 +83,26 @@
             // Videoprojecteurs
             // 
             this.Videoprojecteurs.FormattingEnabled = true;
-            this.Videoprojecteurs.Location = new System.Drawing.Point(886, 124);
+            this.Videoprojecteurs.Location = new System.Drawing.Point(848, 53);
             this.Videoprojecteurs.Name = "Videoprojecteurs";
             this.Videoprojecteurs.Size = new System.Drawing.Size(121, 21);
             this.Videoprojecteurs.TabIndex = 3;
             // 
-            // Videoprojecteur
+            // Date_relevé
             // 
-            this.Videoprojecteur.Location = new System.Drawing.Point(433, 519);
-            this.Videoprojecteur.Name = "Videoprojecteur";
-            this.Videoprojecteur.Size = new System.Drawing.Size(100, 20);
-            this.Videoprojecteur.TabIndex = 4;
-            this.Videoprojecteur.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Date_relevé.Location = new System.Drawing.Point(886, 209);
+            this.Date_relevé.Name = "Date_relevé";
+            this.Date_relevé.Size = new System.Drawing.Size(100, 20);
+            this.Date_relevé.TabIndex = 5;
+            this.Date_relevé.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // Date_releve
+            // Heures_Lampe
             // 
-            this.Date_releve.Location = new System.Drawing.Point(614, 519);
-            this.Date_releve.Name = "Date_releve";
-            this.Date_releve.Size = new System.Drawing.Size(100, 20);
-            this.Date_releve.TabIndex = 5;
-            this.Date_releve.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // Nombre_heures
-            // 
-            this.Nombre_heures.Location = new System.Drawing.Point(800, 518);
-            this.Nombre_heures.Name = "Nombre_heures";
-            this.Nombre_heures.Size = new System.Drawing.Size(100, 20);
-            this.Nombre_heures.TabIndex = 6;
-            this.Nombre_heures.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Heures_Lampe.Location = new System.Drawing.Point(886, 246);
+            this.Heures_Lampe.Name = "Heures_Lampe";
+            this.Heures_Lampe.Size = new System.Drawing.Size(100, 20);
+            this.Heures_Lampe.TabIndex = 6;
+            this.Heures_Lampe.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Liste_périphériques
             // 
@@ -108,7 +115,7 @@
             // Ordinateurs
             // 
             this.Ordinateurs.FormattingEnabled = true;
-            this.Ordinateurs.Location = new System.Drawing.Point(1085, 123);
+            this.Ordinateurs.Location = new System.Drawing.Point(999, 53);
             this.Ordinateurs.Name = "Ordinateurs";
             this.Ordinateurs.Size = new System.Drawing.Size(121, 21);
             this.Ordinateurs.TabIndex = 8;
@@ -116,30 +123,164 @@
             // Choix_colonne
             // 
             this.Choix_colonne.FormattingEnabled = true;
-            this.Choix_colonne.Items.AddRange(new object[] {
-            "Périphérique",
-            "Salle",
-            "Adresse ip",
-            "Vidéoprojecteur",
-            "Bandeau",
-            "Port"});
-            this.Choix_colonne.Location = new System.Drawing.Point(378, 13);
+            this.Choix_colonne.Location = new System.Drawing.Point(409, 53);
             this.Choix_colonne.Name = "Choix_colonne";
             this.Choix_colonne.Size = new System.Drawing.Size(121, 21);
             this.Choix_colonne.TabIndex = 9;
             this.Choix_colonne.SelectedIndexChanged += new System.EventHandler(this.Choix_colonne_SelectedIndexChanged);
             // 
+            // Switch
+            // 
+            this.Switch.Location = new System.Drawing.Point(479, 173);
+            this.Switch.Name = "Switch";
+            this.Switch.Size = new System.Drawing.Size(100, 20);
+            this.Switch.TabIndex = 10;
+            this.Switch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(479, 209);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(100, 20);
+            this.Port.TabIndex = 11;
+            this.Port.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Salle
+            // 
+            this.Salle.Location = new System.Drawing.Point(479, 246);
+            this.Salle.Name = "Salle";
+            this.Salle.Size = new System.Drawing.Size(100, 20);
+            this.Salle.TabIndex = 12;
+            this.Salle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Bandeau
+            // 
+            this.Bandeau.Location = new System.Drawing.Point(479, 283);
+            this.Bandeau.Name = "Bandeau";
+            this.Bandeau.Size = new System.Drawing.Size(100, 20);
+            this.Bandeau.TabIndex = 13;
+            this.Bandeau.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // VLAN
+            // 
+            this.VLAN.Location = new System.Drawing.Point(479, 319);
+            this.VLAN.Name = "VLAN";
+            this.VLAN.Size = new System.Drawing.Size(100, 20);
+            this.VLAN.TabIndex = 14;
+            this.VLAN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Périphérique
+            // 
+            this.Périphérique.Location = new System.Drawing.Point(479, 358);
+            this.Périphérique.Name = "Périphérique";
+            this.Périphérique.Size = new System.Drawing.Size(100, 20);
+            this.Périphérique.TabIndex = 15;
+            this.Périphérique.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Modèle_périphérique
+            // 
+            this.Modèle_périphérique.Location = new System.Drawing.Point(479, 396);
+            this.Modèle_périphérique.Name = "Modèle_périphérique";
+            this.Modèle_périphérique.Size = new System.Drawing.Size(100, 20);
+            this.Modèle_périphérique.TabIndex = 16;
+            this.Modèle_périphérique.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Type
+            // 
+            this.Type.Location = new System.Drawing.Point(479, 433);
+            this.Type.Name = "Type";
+            this.Type.Size = new System.Drawing.Size(100, 20);
+            this.Type.TabIndex = 17;
+            this.Type.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Adresse_ip
+            // 
+            this.Adresse_ip.Location = new System.Drawing.Point(479, 472);
+            this.Adresse_ip.Name = "Adresse_ip";
+            this.Adresse_ip.Size = new System.Drawing.Size(100, 20);
+            this.Adresse_ip.TabIndex = 18;
+            this.Adresse_ip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Imprimante
+            // 
+            this.Imprimante.Location = new System.Drawing.Point(479, 508);
+            this.Imprimante.Name = "Imprimante";
+            this.Imprimante.Size = new System.Drawing.Size(100, 20);
+            this.Imprimante.TabIndex = 19;
+            this.Imprimante.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Port_imprimante
+            // 
+            this.Port_imprimante.Location = new System.Drawing.Point(479, 545);
+            this.Port_imprimante.Name = "Port_imprimante";
+            this.Port_imprimante.Size = new System.Drawing.Size(100, 20);
+            this.Port_imprimante.TabIndex = 20;
+            this.Port_imprimante.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Type_imprimante
+            // 
+            this.Type_imprimante.Location = new System.Drawing.Point(479, 583);
+            this.Type_imprimante.Name = "Type_imprimante";
+            this.Type_imprimante.Size = new System.Drawing.Size(100, 20);
+            this.Type_imprimante.TabIndex = 21;
+            this.Type_imprimante.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Vidéoprojecteur
+            // 
+            this.Vidéoprojecteur.Location = new System.Drawing.Point(886, 172);
+            this.Vidéoprojecteur.Name = "Vidéoprojecteur";
+            this.Vidéoprojecteur.Size = new System.Drawing.Size(100, 20);
+            this.Vidéoprojecteur.TabIndex = 22;
+            // 
+            // Observations
+            // 
+            this.Observations.Location = new System.Drawing.Point(886, 282);
+            this.Observations.Name = "Observations";
+            this.Observations.Size = new System.Drawing.Size(100, 20);
+            this.Observations.TabIndex = 23;
+            this.Observations.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Modèle_Lampe
+            // 
+            this.Modèle_Lampe.Location = new System.Drawing.Point(886, 319);
+            this.Modèle_Lampe.Name = "Modèle_Lampe";
+            this.Modèle_Lampe.Size = new System.Drawing.Size(100, 20);
+            this.Modèle_Lampe.TabIndex = 24;
+            this.Modèle_Lampe.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(614, 371);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 452);
+            this.panel1.TabIndex = 25;
+            // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1322, 648);
+            this.ClientSize = new System.Drawing.Size(1322, 835);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Modèle_Lampe);
+            this.Controls.Add(this.Observations);
+            this.Controls.Add(this.Vidéoprojecteur);
+            this.Controls.Add(this.Type_imprimante);
+            this.Controls.Add(this.Port_imprimante);
+            this.Controls.Add(this.Imprimante);
+            this.Controls.Add(this.Adresse_ip);
+            this.Controls.Add(this.Type);
+            this.Controls.Add(this.Modèle_périphérique);
+            this.Controls.Add(this.Périphérique);
+            this.Controls.Add(this.VLAN);
+            this.Controls.Add(this.Bandeau);
+            this.Controls.Add(this.Salle);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.Switch);
             this.Controls.Add(this.Choix_colonne);
             this.Controls.Add(this.Ordinateurs);
             this.Controls.Add(this.Liste_périphériques);
-            this.Controls.Add(this.Nombre_heures);
-            this.Controls.Add(this.Date_releve);
-            this.Controls.Add(this.Videoprojecteur);
+            this.Controls.Add(this.Heures_Lampe);
+            this.Controls.Add(this.Date_relevé);
             this.Controls.Add(this.Videoprojecteurs);
             this.Controls.Add(this.Liste_départ);
             this.Controls.Add(this.Salles);
@@ -158,12 +299,27 @@
         private System.Windows.Forms.ComboBox Salles;
         private System.Windows.Forms.ListBox Liste_départ;
         private System.Windows.Forms.ComboBox Videoprojecteurs;
-        private System.Windows.Forms.TextBox Videoprojecteur;
-        private System.Windows.Forms.TextBox Date_releve;
-        private System.Windows.Forms.TextBox Nombre_heures;
+        private System.Windows.Forms.TextBox Date_relevé;
+        private System.Windows.Forms.TextBox Heures_Lampe;
         private System.Windows.Forms.ListBox Liste_périphériques;
         private System.Windows.Forms.ComboBox Ordinateurs;
         private System.Windows.Forms.ComboBox Choix_colonne;
+        private System.Windows.Forms.TextBox Switch;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.TextBox Salle;
+        private System.Windows.Forms.TextBox Bandeau;
+        private System.Windows.Forms.TextBox VLAN;
+        private System.Windows.Forms.TextBox Périphérique;
+        private System.Windows.Forms.TextBox Modèle_périphérique;
+        private System.Windows.Forms.TextBox Type;
+        private System.Windows.Forms.TextBox Adresse_ip;
+        private System.Windows.Forms.TextBox Imprimante;
+        private System.Windows.Forms.TextBox Port_imprimante;
+        private System.Windows.Forms.TextBox Type_imprimante;
+        private System.Windows.Forms.TextBox Vidéoprojecteur;
+        private System.Windows.Forms.TextBox Observations;
+        private System.Windows.Forms.TextBox Modèle_Lampe;
+        public System.Windows.Forms.Panel panel1;
     }
 }
 
--- Réseau informatique Saint Jacques/Principal.cs
+++ Réseau informatique Saint Jacques/Principal.cs
@@ -9,23 +9,25 @@ namespace Réseau_informatique_Saint_Jacques
     public partial class Principal : Form
     {
         private string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Reseau St Jacques.accdb";
-
+        int n = 0;
+        int i = 0;
+        int j = 10;
+        int k = 0;
         public Principal()
         {
-            InitializeComponent();
-            //Textbox_Videoprojecteur();
+            InitializeComponent();            
         }
 
         private void Form1_Load(object sender, System.EventArgs e)
-        {
-            Choix_colonne.SelectedItem = "Périphérique";
+        {            
+            Combobox_Colonnes();
+            Choix_colonne.SelectedItem = "Salle";
             Combobox_tables();
             Combobox_imprimantes();
             //Listbox_Départ_Condition("Périphérique", "Brassage", "Type", "Ordinateur");
             Listbox_Départ(Choix_colonne.SelectedItem.ToString(), "Brassage");
             Combobox_Videoprojecteurs();
-            Combobox_ordinateurs();
-            
+            Combobox_ordinateurs();            
         }
 
         private void Combobox_imprimantes()
@@ -55,14 +57,41 @@ namespace Réseau_informatique_Saint_Jacques
             OleDbConnection database = new OleDbConnection(connectionString);
             database.Open();
             DataTable userTables = database.GetSchema("Tables");
-
-            List<string> tableNames = new List<string>();
+            
             for (int i = 0; i < userTables.Rows.Count; i++)
                 if (!(userTables.Rows[i][2].ToString()).Contains("MS"))
                     Tables.Items.Add(userTables.Rows[i][2].ToString());
             database.Close();
             Tables.SelectedIndex = 0;
         }
+
+        private void Combobox_Colonnes()
+        {
+            string requete = "SELECT * FROM BRASSAGE";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(requete, connectionString);
+            DataTable result = new DataTable();
+            adapter.Fill(result);
+           
+            foreach (DataColumn item in result.Columns)
+            {                
+                Choix_colonne.Items.Add(item.ColumnName);
+                Choix_colonne.Sorted = true;
+            }            
+        } 
+        
+        private void Effacer_Textbox()
+        {
+            string requete = "SELECT * FROM BRASSAGE";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(requete, connectionString);
+            DataTable result = new DataTable();
+            adapter.Fill(result);
+
+            foreach (DataColumn item in result.Columns)
+            {
+                if (item.ColumnName != "ID")
+                this.Controls[item.ColumnName].Text = "";
+            }
+        }       
 
         private void Combobox_Videoprojecteurs()
         {
@@ -88,9 +117,32 @@ namespace Réseau_informatique_Saint_Jacques
 
         private void Liste_salles_SelectedIndexChanged(object sender, System.EventArgs e)
         {
-            Textbox_Videoprojecteur();
-            Textbox_Date_Videoprojecteur();
-            Textbox_Heure_Videoprojecteur();
+            Effacer_Textbox();
+            Supprimer_Textbox();            
+            Textbox_Colonne("Vidéoprojecteur");
+            Textbox_Colonne("Date_Relevé");
+            Textbox_Colonne("Heures_Lampe");
+            Textbox_Colonne("Observations");
+            Textbox_Colonne("Modèle_Lampe");
+            j = j + 30; i = 0;
+            Textbox_Colonne("Switch");
+            j = j + 30; i = 0;
+            Textbox_Colonne("Bandeau");
+            j = j + 30; i = 0;
+            Textbox_Colonne("Port");
+            j = j + 30; i = 0;
+            //Textbox_Colonne("Salle");           
+            Textbox_Colonne("VLAN");
+            j = j + 30; i = 0;
+            Textbox_Colonne("Périphérique");
+            Textbox_Colonne("Modèle_périphérique");
+            j = j + 30; i = 0;
+            //Textbox_Colonne("Type");
+            Textbox_Colonne("Adresse_ip");
+            j = j + 30; i = 0;
+            Textbox_Colonne("imprimante");
+            Textbox_Colonne("Port_imprimante");
+            Textbox_Colonne("Type_imprimante");            
         }
 
         private void Listbox_Départ(string colonne, string table)
@@ -103,53 +155,61 @@ namespace Réseau_informatique_Saint_Jacques
             Liste_départ.DisplayMember = colonne;
             Liste_départ.ValueMember = colonne;
         }
-
-        private void Textbox_Videoprojecteur()
+        private void Textbox_Colonne(string colonne)
         {
             OleDbConnection database = new OleDbConnection(connectionString);
-            string requete = "SELECT Vidéoprojecteur FROM Brassage WHERE Salle = '" + Liste_départ.GetItemText(Liste_départ.SelectedItem) + "'";
+            string requete = "SELECT " + colonne + " FROM Brassage WHERE " + Choix_colonne.SelectedItem.ToString() + " = '" + Liste_départ.GetItemText(Liste_départ.SelectedItem) + "'";
             database.Open();
             OleDbCommand command = new OleDbCommand(requete, database);
-            OleDbDataReader reader = command.ExecuteReader();
+            OleDbDataReader reader = command.ExecuteReader();           
+            
             while (reader.Read())
             {
-                Videoprojecteur.Text = reader[0].ToString();
+                if (reader[0].ToString() != "")
+                {
+                    if (colonne == "Date_Relevé") { this.Controls[colonne].Text = Convert.ToDateTime(reader[0].ToString()).ToString("d/MM/yyyy"); }
+                    this.Controls[colonne].Text = reader[0].ToString();
+                    TextBox[] textBoxes = new TextBox[500];
+                    textBoxes[n] = new TextBox();
+                    textBoxes[n].Name = colonne.ToString() + n.ToString();
+                    textBoxes[n].Top = j;
+                    textBoxes[n].Left = 100 + (100 * i);
+                    if (textBoxes[n].Left > 500) { j = j + 30; i = 0; }                   
+                    textBoxes[n].Text = reader[0].ToString();
+                    this.panel1.Controls.Add(textBoxes[n]);                   
+                    n++;
+                    i++;
+                }
+                
             }
+            
             reader.Close();
             database.Close();
-        }
-
-        private void Textbox_Heure_Videoprojecteur()
+        } 
+        
+        private void Supprimer_Textbox()
         {
-            OleDbConnection database = new OleDbConnection(connectionString);
-            string requete = "SELECT Heures_Lampe FROM Brassage WHERE Salle = '" + Liste_départ.GetItemText(Liste_départ.SelectedItem) + "'";
-            database.Open();
-            OleDbCommand command = new OleDbCommand(requete, database);
-            OleDbDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                Nombre_heures.Text = reader[0].ToString();
-            }
-            reader.Close();
-            database.Close();
-        }
-
+            panel1.Controls.Clear();
+            n = 0;
+            i = 0;
+            j = 10;            
+        }       
+       
         private void Textbox_Date_Videoprojecteur()
         {
             OleDbConnection database = new OleDbConnection(connectionString);
-            string requete = "SELECT Date_Relevé FROM Brassage WHERE Salle = '" + Liste_départ.GetItemText(Liste_départ.SelectedItem) + "'";
+            string requete = "SELECT Date_Relevé FROM Brassage WHERE " + Choix_colonne.SelectedItem.ToString() + " = '" + Liste_départ.GetItemText(Liste_départ.SelectedItem) + "'";
             database.Open();
             OleDbCommand command = new OleDbCommand(requete, database);
             OleDbDataReader reader = command.ExecuteReader();
             while (reader.Read())
             {
                 if ((reader[0].ToString().Length) > 0)
-                    Date_releve.Text = Convert.ToDateTime(reader[0].ToString()).ToString("d/MM/yyyy");
-                else { Date_releve.Text = reader[0].ToString(); }
+                    Date_relevé.Text = Convert.ToDateTime(reader[0].ToString()).ToString("d/MM/yyyy");                
             }
             reader.Close();
             database.Close();
-        }
+        }        
 
         private void Choix_colonne_SelectedIndexChanged(object sender, EventArgs e)
         {
